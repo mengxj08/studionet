cab22febdf9fd707a999cef92d3d10390b325881
Edmund <edmundmok@outlook.com>
2016-12-15 16:04:35 +0800

    Add winston logger

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var logger = require('./config/logger');
 var cookieParser = require('cookie-parser');
 var passport = require('passport');
 var bodyParser = require('body-parser');
@@ -24,6 +24,8 @@ var supernode = require('./routes/supernode');
 var logs = require('./routes/logs');
 
 var app = express();
+logger.debug("Overriding 'Express' logger");
+app.use(require('morgan')('combined', { "stream": logger.stream }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,8 +33,8 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-if (process.env.NODE_ENV !== 'test')
-  app.use(logger('dev'));
+//if (process.env.NODE_ENV !== 'test')
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
